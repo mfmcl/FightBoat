754fd0f15ab550363bce0fa4a7b7bb9ab18466c2
mfmcl <michael.mclean@student.leaf.academy>
2021-02-01 18:24:45 +0100

    -

--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,12 @@
 public class Grid {
     
+    Grid(int size) {
+        
+        boolean[][] grid = new boolean[size][size];
+    }
+
+    public static void main(String[] args) {
+        Grid g = new Grid(4);
+        System.out.println(g);
+    }
 }
