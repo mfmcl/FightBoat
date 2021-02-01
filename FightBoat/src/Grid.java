7473cef7369930539f0eba20efe30c943d148b5f
mfmcl <michael.mclean@student.leaf.academy>
2021-02-01 19:11:17 +0100

    -

--- FightBoat/src/Grid.java
+++ FightBoat/src/Grid.java
@@ -2,9 +2,13 @@ package Fightboat.src;
 
 public class Grid {
     
+    private boolean[][]
     Grid(int size) {
         
         boolean[][] grid = new boolean[size][size];
     }
 
+    public boolean[][] getGrid() {
+        return grid;
+    }
 }
